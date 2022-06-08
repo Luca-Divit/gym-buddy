bb9819010951c56e5c8285f35276fc8393041d37
afeez oloyede <oloyedeafeez@363photography.org>
2022-06-08 12:02:17 -0700

    saving changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,12 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @users = @users.where(partner_gender_preference: current_user.partner_gender_preference)
+    # @users = User.all
     # prefereces.match just one sport or one day
     #@users = User.where(location, preferences)
   end
+
+
+
 end
