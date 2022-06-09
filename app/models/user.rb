befecabcdc01607fa1d46e70031982c3638fa0ba
Luca Di Vittorio <luca.div@hotmail.com>
2022-06-09 18:33:16 +0100

    finished dashboard and accept decline methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :user_activities
   has_many :activities, through: :user_activities
   has_one_attached :photo
+  has_many :matches
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
