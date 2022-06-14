6d749cd75e7e871eeb7492213b32393d36c2a2fb
Luca Di Vittorio <luca.div@hotmail.com>
2022-06-14 19:35:25 +0100

    refactored seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,10 +20,10 @@ end
 activity = Activity.all
 
 puts "start creating users"
-200.times do
+250.times do
   start_time1 = rand(5..23).to_i
   end_time1 = start_time1.to_i + rand(1..4).to_i
-  gender = ["male","female"].sample.to_s
+  gender = ["male", "female"].sample.to_s
   if gender == "male"
     file = URI.open("https://res.cloudinary.com/dmmli4rcp/image/upload/v1654870356/gym-buddy-males/#{rand(2..20)}.jpg")
   else
@@ -32,18 +32,34 @@ puts "start creating users"
   user = User.new(
     email: Faker::Internet.email,
     password: "123456",
-    first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     gender: gender,
     address: ["London", "Shoreditch", "Haggerston", "Hoxton", "Islington", "Bethnal Green", "Islington", "Dalston", "Hackney", "Barbican"].sample.to_s,
-    age: rand(15..50).to_i,
+    age: rand(15..40).to_i,
     level_of_fitness: ["Beginner","Intermediate","Advanced"].sample.to_s,
     days_available: ["Monday","Tuesday","Wednesday", "Thursday","Friday","Saturday","Sunday"].sample(3),
     start_time: start_time1,
     end_time: end_time1.to_i,
     partner_gender_preference: ["Male","Female","Flexible"].sample,
-    bio: ["Pain is temporary, pride is forever","Shut up and squat!","Forget the glass slippers, princess wear running shoes","Gym is my therapy.","Work. Train. Repeat.","Work hard now, selfie later.","Fit and Fat differ by middle alphabet.","I’m in a good place right now, not emotionally, I am just at the gym.","No pain, no gain. Shut up and train","Change your body by changing your thoughts.","Eat, sleep , gym , repeat." ].sample
+    bio: [
+      "Pain is temporary, pride is forever",
+      "Shut up and squat!",
+      "Forget the glass slippers, princess wear running shoes",
+      "Gym is my therapy.",
+      "Work. Train. Repeat.",
+      "Work hard now, selfie later.",
+      "Fit and Fat differ by middle alphabet.",
+      "I’m in a good place right now, not emotionally, I am just at the gym.",
+      "No pain, no gain. Shut up and train",
+      "Change your body by changing your thoughts.",
+      "Eat, sleep , gym , repeat."
+    ].sample
   )
+  if user.gender == "male"
+    user.first_name = Faker::Name.male_first_name
+  else
+    user.first_name = Faker::Name.female_first_name
+  end
   user.geocode
   user.photos.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
   user.save
@@ -52,7 +68,7 @@ users = User.all
 
 puts "start creating user_activities"
 users.each do |user|
-  3.times do
+  2.times do
   UserActivity.create!(
     user_id: user.id,
     activity_id: activity.sample.id
